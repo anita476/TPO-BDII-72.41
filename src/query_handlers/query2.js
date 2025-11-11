411beb9c1a74971060b015d8bf45ca61ba3d8363
juane993 <jolivamorroni@itba.edu.ar>
2025-11-11 11:31:48 -0300

    query 2

---
+++ src/query_handlers/query2.js
@@ -0,0 +1,41 @@
+
+
+import siniestro from "../models/Siniestro.js";
+
+export async function query2(req,res) {
+    const sini = siniestro.aggregate([
+        {
+            $match: {
+                "estado": "Abierto"
+            }
+        },
+        {
+            $lookup:{
+                from: "clientes",
+                localField: "nro_poliza",
+                foreignField: "polizas.nro_poliza",
+
+                as: "cliente"
+            }
+        },
+        {
+            $project:{
+                "tipo": 1,
+                "monto_estimado": 1,
+                "cliente.nombre":1,
+                "cliente.apellido":1,
+                "cliente.email":1,
+                "cliente.ciudad":1,
+                "cliente.direccion":1,
+                "cliente.dni":1,
+                "cliente.id_cliente":1,
+                "cliente.provincia":1,
+                "cliente.telefono":1,
+            }
+        }
+    ])
+    const resp = await sini.exec()
+    console.log("RES\n")
+    console.log(resp)
+    res.json(resp)
+}
--- src/routes/queryRoutes.js
+++ src/routes/queryRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import Cliente from "../models/Cliente.js";
 import {query1} from "../query_handlers/query1.js";
+import {query2} from "../query_handlers/query2.js";
 
 const router = express.Router();
 
 // Query 1 endpoint
 router.get('/query1', query1)
+router.get('/query2',query2)
 
 
 
